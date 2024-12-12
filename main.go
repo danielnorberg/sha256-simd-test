287277eecea6a0272105b5e6daea4ae7f1e2da7c
Daniel Norberg <dano@modal.com>
2024-12-12 20:26:11 +0000

    server

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 
 	"github.com/minio/sha256-simd"
@@ -16,17 +17,31 @@ func check(e error) {
 
 func main() {
 	shaWriter := sha256.New()
+	file, err := os.Open(os.Args[2])
+	check(err)
 
 	switch os.Args[1] {
 	case "simple":
-		file, err := os.Open(os.Args[2])
-		check(err)
 		io.Copy(shaWriter, file)
 		fmt.Printf("%x", shaWriter.Sum(nil))
 	case "server":
 		server := sha256.NewAvx512Server()
 		h512 := sha256.NewAvx512(server)
-		h512.Write(fileBlock)
+		buf := make([]byte, 2<<20)
+		for {
+			n, err := file.Read(buf)
+			// fmt.Printf("read %d, %v\n", n, err)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatal(err)
+			}
+			h512.Write(buf[:n])
+			if err != nil && err != io.EOF {
+				log.Fatal(err)
+			}
+		}
 		fmt.Printf("%x", h512.Sum([]byte{}))
 	}
 }
